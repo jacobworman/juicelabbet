ed270fe0dd68b5c787d1f6439a77bf07fa76ba52
jacobworman <jacobworman@gmail.com>
2018-06-17 15:17:03 +0000

    fds

--- html/wp-content/themes/elara/searchform.php
+++ html/wp-content/themes/elara/searchform.php
@@ -9,6 +9,5 @@
 	<input type="search" class="search-field" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" placeholder="<?php esc_attr_e( 'Search', 'elara' ); ?>" />
 	<button type="submit" class="search-submit">
 		<?php elara_fontawesome_icon( 'search' ); ?>
-		<span><?php esc_html_e( 'Search', 'elara' ); ?></span>
 	</button>
 </form>

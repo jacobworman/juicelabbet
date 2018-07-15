9a50c923e918a766d53a1c1abbc140a3f0dd947d
jacobworman <jacobworman@gmail.com>
2018-07-15 13:21:19 +0200

    jfk

--- html/wp-content/themes/elara/inc/template-tags.php
+++ html/wp-content/themes/elara/inc/template-tags.php
@@ -42,7 +42,8 @@ if ( ! function_exists( 'elara_entry_thumbnail' ) ) :
 				<?php endif; ?>
 
 				<?php if ( has_post_thumbnail() ) : ?>
-					<?php the_post_thumbnail( $size, array( 'alt' => get_the_title(), 'class' => 'img-responsive' ) ); ?>
+					<?php the_post_thumbnail( null, array( 'alt' => get_the_title(), 'class' => 'img-responsive' ) ); ?>
+
 				<?php elseif ( $elara_example_content == 1 ) : ?>
 					<img src="<?php echo esc_url( elara_get_sample( $size ) ); ?>"  class="img-responsive" />
 				<?php endif; ?>
@@ -50,7 +51,7 @@ if ( ! function_exists( 'elara_entry_thumbnail' ) ) :
 				<?php if ( 'video' === get_post_format() && elara_get_first_embed_media( get_the_ID() ) && $show_modal ) : ?>
 						<span class="screen-reader-text"><?php esc_html_e( 'Launch video modal', 'elara' ); ?></span>
 						<span class="entry-play-icon">
-							<img  height="" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/play.png" alt="<?php esc_attr_e( 'Launch video modal', 'elara' ); ?>">
+							<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/play.png" alt="<?php esc_attr_e( 'Launch video modal', 'elara' ); ?>">
 						</span>
 					</a>
 				<?php else : ?>

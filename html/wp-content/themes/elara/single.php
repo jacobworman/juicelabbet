21645d7b552c60d284ffe190c70076641ef6cbda
jacobworman <jacobworman@gmail.com>
2018-07-15 14:12:47 +0200

    hfjk

--- html/wp-content/themes/elara/single.php
+++ html/wp-content/themes/elara/single.php
@@ -35,6 +35,22 @@ if ( $elara_posts_sidebar ) {
 										endif;
 									endif;
 								?>
+								<!-- här -->
+								<div class=" <?php echo esc_attr( $elara_row_class ); ?>">
+								<header>
+									<h2><?php elara_entry_title(); ?></h2>
+									<p><?php elara_entry_author(); ?>
+										<?php elara_entry_categories(); ?>
+											<?php elara_entry_separator( 'categories-date' ); ?>
+											<?php elara_entry_date(); ?></p>
+								</header>
+								 <?php
+										if ( $elara_posts_sidebar_position == 'top' && $elara_posts_sidebar ) :
+											if ( $elara_posts_featured_image_show ) :
+												elara_entry_thumbnail( 'elara-slider' );
+											endif;
+										endif;
+									?>
 
 					</div>
 				</section>

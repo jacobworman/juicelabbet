11b78f017f16b228138ba15da6e5895f744a8175
jacobworman <jacobworman@gmail.com>
2018-07-15 14:03:51 +0200

    pure

--- html/wp-content/themes/elara/category.php
+++ html/wp-content/themes/elara/category.php
@@ -8,25 +8,26 @@
  */
 get_header(); ?>
 
-<h1>YES6!!</h1>
-<div class="default-background-color">
-	<main class="main" role="main">
-		<div class="wrapper">
 
-				<div class="row">
-					<div class="<?php echo esc_attr( elara_set_main_class() ); ?>">
-						<div class="archive-header">
-							<h2><?php single_cat_title(); ?></h2>
-							<?php the_archive_description(); ?>
+<!-- Main -->
+				<section id="main">
+					<div class="container">
+						<div class="row">
+							<div class="col-12">
+								<!-- Portfolio -->
+									<section>
+										<header class="major">
+											<h2><?php single_cat_title(); ?></h2>
+											<p><?php the_archive_description(); ?></p>
+										</header>
+										<div class="row">
+											<?php get_template_part( 'parts/feed' ); ?>
+										</div>
+									</section>
+
+							</div>
 						</div>
-						<?php get_template_part( 'parts/feed' ); ?>
-					</div><!-- <?php echo esc_attr( elara_set_main_class() ); ?> -->
-
-					<?php elara_show_sidebar(); ?>
-				</div><!-- row -->
-
-		</div><!-- wrapper -->
-	</main>
-</div><!-- default-background-color -->
+					</div>
+				</section>
 
 <?php get_footer();
--- html/wp-content/themes/elara/single.php
+++ html/wp-content/themes/elara/single.php
@@ -19,66 +19,41 @@ if ( $elara_posts_sidebar ) {
 	$elara_row_class = 'post-sidebar-off';
 }
 ?>
-<h1>YES3!!</h1>
-<div class="default-background-color">
-	<main class="main" role="main">
-		<div class="wrapper">
-				<?php
-					if ( $elara_posts_sidebar_position == 'bottom' || ! $elara_posts_sidebar ) :
-						/**
-						 * Featured image
-						 */
-						if ( $elara_posts_featured_image_show ) :
-							elara_entry_thumbnail( 'elara-slider' );
-						endif;
-					endif;
-				?>
-				<div class="row <?php echo esc_attr( $elara_row_class ); ?>">
-					<div class="col-md-9 col-xs-12 entry-singular-wrapper">
-						<article id="post-<?php the_ID(); ?>" <?php post_class( 'entry-singular' ); ?>>
-							<?php
-								if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-									<footer class="entry-meta">
-										<div>
-											<?php elara_entry_categories(); ?>
+<section id="main">
+					<div class="container">
+
+						<!-- Content -->
+							<article class="box post">
+								<?php elara_entry_thumbnail( 'elara-slider' ); ?>
+								<header>
+									<h2><?php elara_entry_title(); ?></h2>
+									<p><?php elara_entry_categories(); ?>
 											<?php elara_entry_separator( 'categories-date' ); ?>
-											<?php elara_entry_date(); ?>
-										</div>
-									</footer>
-
-									<header class="entry-header">
-										<?php elara_entry_title(); ?>
-										<?php elara_entry_author(); ?>
-									</header>
-
-                                    <?php
-										if ( $elara_posts_sidebar_position == 'top' && $elara_posts_sidebar ) :
-											if ( $elara_posts_featured_image_show ) :
-												elara_entry_thumbnail( 'elara-slider' );
-											endif;
-										endif;
-									?>
-
-									<div class="entry-content clearfix">
-										<?php the_content(); ?>
+											<?php elara_entry_date(); ?></p>
+								</header>
+								
+								<section>
+									<?php the_content(); ?>
 										<?php
 											wp_link_pages( array(
 												'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'elara' ),
 												'after'  => '</div>',
 											));
 										?>
-									</div>
-
+								</section>
+								<section>
 									<footer class="entry-meta">
+										<h3>Relevanta ingredienser</h3>
 										<?php
 											if ( $elara_posts_meta_show && $elara_posts_tags_show && has_tag() ) :
 												the_tags( '<h3 class="section-title">' . esc_html__( 'Tags', 'elara' ) . '</h3><ul><li class="list-item-separators">', '</li><li class="list-item-separators">', '</li></ul>' );
 											endif;
 										?>
 									</footer>
-
-								<?php
+								</section>
+								<section>
+									<?php
 									/**
 									 * Post navigation
 									 */
@@ -89,13 +64,9 @@ if ( $elara_posts_sidebar ) {
 									endif;
 								endwhile; endif;
 							?>
-						</article><!-- #post-<?php the_ID(); ?> -->
-					</div><!-- col-md-9 col-xs-12 entry-singular-wrapper -->
+								</section>
+							</article>
 
-					<?php elara_show_sidebar(); ?>
-				</div><!-- row -->
-		</div><!-- wrapper -->
-	</main>
-</div><!-- default-background-color -->
-
+					</div>
+				</section>
 <?php get_footer();
